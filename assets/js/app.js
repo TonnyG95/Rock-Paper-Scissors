992af52f8a05a316844eaab27d8365f26a4fff02
TonnyG95 <galic_antonio@hotmail.com>
2021-12-28 15:45:56 +0000

    added a function that captures icons onclick

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,16 @@
-const imageTags = document.querySelectorAll("img")
+// get all the image tags
+const imageTags = document.querySelectorAll('img');
+// variables for game stats
 let computerWin = 0;
-let userWin= 0;
+let userWin = 0;
+// console.log(imageTags);
+// find out the img element on which a click is performed
+for(let i = 0; i < imageTags.length; i++){
+    imageTags[i].onclick = function(e){
+        
+        let user = e.target.id; // user optioin
+        let computer = GetComputerChoice(); // computer option
+
+        DecideWinner(user, computer);
+    }
+}
