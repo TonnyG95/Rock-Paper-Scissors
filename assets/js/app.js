99e9eeac396cb904277567c00cfa54c34ad84487
TonnyG95 <galic_antonio@hotmail.com>
2021-12-28 14:01:57 +0000

    finish htlm structure and added some styling

---
+++ assets/js/app.js
@@ -0,0 +1,4 @@
+const computerChoice = document.getElementById("computer-choise")
+const userChoice = document.getElementById("user-choise")
+const resultDisplay = document.getElementById("result")
+const possibleChoices = document.querySelectorAll("button")
